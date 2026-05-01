7f3bac1ddce6cbb74b92088f0a246d6722310eea
xatandcatch-blip <xatandcatch@gmail.com>
2026-05-01 13:05:46 +0300

    Add BuildManager class

---
+++ BuildManager.kt
@@ -0,0 +1,10 @@
+// BuildManager.kt
+
+import java.io.File
+
+class BuildManager {
+    fun compileSources(kotlinSources: List<File>, javaSources: List<File>): List<File> {
+        // Logic to compile Kotlin/Java sources to DEX
+        return listOf() // Return list of compiled DEX files
+    }
+}
